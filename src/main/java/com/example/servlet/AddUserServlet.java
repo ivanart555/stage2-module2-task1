0d873379ab29ac82523e3e21eb5e60a5a02ef9d5
Ivan Artsiushyn <ivanart555@gmail.com>
2023-08-02 21:05:44 +0300

    Add logging amn empty checks

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -9,28 +9,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet("/add")
 public class AddUserServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(AddUserServlet.class.getName());
+    private final String JSP_ADD = "/jsp/add.jsp";
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        getServletContext().getRequestDispatcher("/jsp/add.jsp").forward(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            req.getRequestDispatcher(JSP_ADD).forward(req, resp);
+        } catch (ServletException | IOException e) {
+            log.info("Failed to forward Get request! " + e.getMessage());
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
 
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
 
-        User user = new User(firstName, lastName);
+        if (!firstName.isEmpty() && !lastName.isEmpty()) {
+            User user = new User(firstName, lastName);
+            Warehouse.getInstance().addUser(user);
+            req.setAttribute("user", user);
 
-        Warehouse.getInstance().addUser(user);
-
-        req.setAttribute("user", user);
-
-        req.getRequestDispatcher("/jsp/add.jsp").forward(req, resp);
-
+            try {
+                req.getRequestDispatcher(JSP_ADD).forward(req, resp);
+            } catch (ServletException | IOException e) {
+                log.info("Failed to forward Post request! " + e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -10,18 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Set;
+import java.util.logging.Logger;
 
 @WebServlet("/users")
 public class GetUsersServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        resp.setContentType("text/html;charset=UTF-8");
+    private static final Logger log = Logger.getLogger(AddUserServlet.class.getName());
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         Set<User> users = Warehouse.getInstance().getUsers();
 
         req.setAttribute("users", users);
 
-        req.getRequestDispatcher("/jsp/users.jsp").forward(req, resp);
+        try {
+            req.getRequestDispatcher("/jsp/users.jsp").forward(req, resp);
+        } catch (ServletException | IOException e) {
+            log.info("Failed to forward Get request! " + e.getMessage());
+        }
 
     }
 }
